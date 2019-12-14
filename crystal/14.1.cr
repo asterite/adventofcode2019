9b146af4fe59eef5cf63028ae8ee82f4f574d911
Ary Borenszweig <asterite@gmail.com>
2019-12-13 23:47:35 -0800

    Put comments on day 14

--- crystal/14.1.cr
+++ crystal/14.1.cr
@@ -1,7 +1,10 @@
+# This is a reaction like:
+# 7 A, 1 E => 1 FUEL
 record Reaction,
   inputs : Array(ChemicalQuantity),
   output : ChemicalQuantity
 
+# This is each chemical quantity in a reaction
 class ChemicalQuantity
   getter name, quantity
 
@@ -14,10 +17,20 @@ class ChemicalQuantity
   end
 end
 
+# This is a chemical's information
 class Chemical
+  # It's name
   getter name
+
+  # What elements this chemical produces (across all reactions)
   getter outputs = Set(Chemical).new
+
+  # The chemical's priority: the biggest the number, the later
+  # it happens in the reaction. ORE has a priority of 0 and
+  # FUEL will have the maximum priority.
   getter priority
+
+  # This is the total quantity needed so far.
   property quantity : Int32
 
   def initialize(@name : String)
@@ -25,6 +38,10 @@ class Chemical
     @quantity = 0
   end
 
+  # To compute the priority we start from ORE and
+  # the outputs' priority will be one more that this
+  # chemical's priority. If it had a previous priority,
+  # the bigger one wins.
   def compute_priority(priority)
     if priority > @priority
       @priority = priority
@@ -33,6 +50,7 @@ class Chemical
   end
 end
 
+# Parse the file into reactions
 reactions = File
   .read_lines("#{__DIR__}/../inputs/14.txt")
   .map do |line|
@@ -42,29 +60,43 @@ reactions = File
     Reaction.new(inputs, output)
   end
 
+# Store all different chemicals by name
 chemicals_by_name = Hash(String, Chemical).new do |h, name|
   h[name] = Chemical.new(name)
 end
 
+# Gather all chemical outputs
 reactions.each do |reaction|
   reaction.inputs.each do |input|
     chemicals_by_name[input.name].outputs << chemicals_by_name[reaction.output.name]
   end
 end
 
+# Get just the Chemicals from the hash
 chemicals = chemicals_by_name.values
 
+# Compute the priority of each element, starting from 0
 chemicals.each &.compute_priority(0)
+
+# Sort by priority, so FUEL will be first and ORE will be last
 chemicals.sort_by! { |chemical| -chemical.priority }
 
+# We need one FUEL to begin with
 chemicals_by_name["FUEL"].quantity = 1
 
+# Now for each chemical, which are sorted by priority,
+# we compute how many inputs we need
 chemicals.each do |chemical|
+  # ORE doesn't need inputs to be produced
   next if chemical.name == "ORE"
 
+  # Find the reaction that produces this chemical
   reaction = reactions.find(&.output.name.==(chemical.name)).not_nil!
 
+  # How many times we need to apply this reaction?
   multiplier = (chemical.quantity / reaction.output.quantity).ceil.to_i
+
+  # Add the quantities that we need directly in each chemical
   reaction.inputs.each do |input|
     chemicals_by_name[input.name].quantity += input.quantity * multiplier
   end
--- crystal/14.2.cr
+++ crystal/14.2.cr
@@ -1,7 +1,10 @@
+# This is a reaction like:
+# 7 A, 1 E => 1 FUEL
 record Reaction,
   inputs : Array(ChemicalQuantity),
   output : ChemicalQuantity
 
+# This is each chemical quantity in a reaction
 class ChemicalQuantity
   getter name, quantity
 
@@ -14,17 +17,33 @@ class ChemicalQuantity
   end
 end
 
+# This is a chemical's information
 class Chemical
+  # It's name
   getter name
+
+  # What elements this chemical produces (across all reactions)
   getter outputs = Set(Chemical).new
+
+  # The chemical's priority: the biggest the number, the later
+  # it happens in the reaction. ORE has a priority of 0 and
+  # FUEL will have the maximum priority.
   getter priority
+
+  # This is the total quantity needed so far.
+  # This is *the* difference between part 1: we use
+  # float because we'll use left overs.
   property quantity : Float64
 
   def initialize(@name : String)
     @priority = 0
-    @quantity = 0.0
+    @quantity = 0
   end
 
+  # To compute the priority we start from ORE and
+  # the outputs' priority will be one more that this
+  # chemical's priority. If it had a previous priority,
+  # the bigger one wins.
   def compute_priority(priority)
     if priority > @priority
       @priority = priority
@@ -33,6 +52,7 @@ class Chemical
   end
 end
 
+# Parse the file into reactions
 reactions = File
   .read_lines("#{__DIR__}/../inputs/14.txt")
   .map do |line|
@@ -42,32 +62,48 @@ reactions = File
     Reaction.new(inputs, output)
   end
 
+# Store all different chemicals by name
 chemicals_by_name = Hash(String, Chemical).new do |h, name|
   h[name] = Chemical.new(name)
 end
 
+# Gather all chemical outputs
 reactions.each do |reaction|
   reaction.inputs.each do |input|
     chemicals_by_name[input.name].outputs << chemicals_by_name[reaction.output.name]
   end
 end
 
+# Get just the Chemicals from the hash
 chemicals = chemicals_by_name.values
 
+# Compute the priority of each element, starting from 0
 chemicals.each &.compute_priority(0)
+
+# Sort by priority, so FUEL will be first and ORE will be last
 chemicals.sort_by! { |chemical| -chemical.priority }
 
+# We need one FUEL to begin with
 chemicals_by_name["FUEL"].quantity = 1
 
+# Now for each chemical, which are sorted by priority,
+# we compute how many inputs we need
 chemicals.each do |chemical|
+  # ORE doesn't need inputs to be produced
   next if chemical.name == "ORE"
 
+  # Find the reaction that produces this chemical
   reaction = reactions.find(&.output.name.==(chemical.name)).not_nil!
 
+  # How many times we need to apply this reaction?
+  # Here we don't round because we'll be using left-over
   multiplier = chemical.quantity / reaction.output.quantity
+
+  # Add the quantities that we need directly in each chemical
   reaction.inputs.each do |input|
     chemicals_by_name[input.name].quantity += input.quantity * multiplier
   end
 end
 
+# At the end we should have the ORE that we need
 puts (1000000000000 / chemicals_by_name["ORE"].quantity).floor.to_i
